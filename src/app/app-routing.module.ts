d90e75cc92621e38dd16c05ad9df52ab312a14c2
izzad24 <afifizzad_mazlan@dell.com>
2019-09-08 15:01:01 +0800

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pie', component: PieChartComponent},
+  { path: 'bar', component: BarChartComponent},
+  { path: 'line', component: LineChartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
